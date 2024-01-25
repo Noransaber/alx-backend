fe06d9cef8f0eba6fc3b7881002a09e29c59435f
Noransaber <noransaber685@gmail.com>
2024-01-25 21:47:45 +0200

    Solving the first task

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -18,24 +18,15 @@ def index_range(page: int, page_size: int) -> tuple:
         startIndex = page - 1
     if page > 1:
         startIndex = page_size * (page - 1)
-    
+
     # Calculating the end index
     endIndex = page * page_size
     # endIndex = preEndIndex - 1
-    
+
     # Push the result in the tuple
     lst = list(result)
     lst.append(startIndex)
     lst.append(endIndex)
     tpl = tuple(lst)
-    
-    return tpl
-    
-    
-res = index_range(1, 7)
-print(type(res))
-print(res)
 
-res = index_range(page=3, page_size=15)
-print(type(res))
-print(res)
+    return tpl
