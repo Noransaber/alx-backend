839f91e37b78729b6afc486fd3d34d7a5a10bbe4
Noransaber <noransaber685@gmail.com>
2024-01-31 11:44:31 +0200

    Solving task 2

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,23 +5,25 @@
 
 from base_caching import BaseCaching
 
-class FIFOCache(BaseCaching):
-  """
-  FIFOCache is a subclass of BaseCaching.
-  """
-  def __init__(self):
-      """
-      Initialize the FIFOCache class.
-      """
-      super().__init__()
 
-  def put(self, key, item):
-      """
-      Put an item in the cache.
-      """
-      if key is not None and item is not None:
-          self.cache_data[key] = item
-          if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-              discarded_key = list(self.cache_data)[0]
-              self.cache_data.pop(discarded_key)
-              print("DISCARD: " + str(discarded_key))
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache is a subclass of BaseCaching.
+    """
+
+    def __init__(self):
+        """
+        Initialize the FIFOCache class.
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Put an item in the cache.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discarded_key = list(self.cache_data)[0]
+                self.cache_data.pop(discarded_key)
+                print("DISCARD: " + str(discarded_key))
